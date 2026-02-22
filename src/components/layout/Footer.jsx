1e1328c303adeaa43d81584114ac4e1676653bca
MonaSweataSK <monasweatask@gmail.com>
2026-02-22 19:42:38 +0530

    footer arc fix with svg

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -19,7 +19,16 @@ export function Footer() {
 
   return (
     <footer className="footer-section" id="footer">
-      <div className="footer-arc-bg"></div>
+      <div className="footer-bg-svg">
+        {/* Desktop SVG background */}
+        <svg className="footer-svg-desktop" viewBox="0 0 1920 346" fill="none" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="none">
+          <path d="M0 346H1920V100.924C1170.28 -35.0035 749.965 -32.272 0 100.924V346Z" fill="#0E1B2C"/>
+        </svg>
+        {/* Mobile SVG background */}
+        <svg className="footer-svg-mobile" viewBox="0 0 375 469" fill="none" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="none">
+          <path d="M-113 468.5H487V100.76C351.076 -34.7924 20.5 -32.3752 -113 100.76V468.5Z" fill="#0E1B2C"/>
+        </svg>
+      </div>
       
       {/* Decorative streaks positioned relative to screen/section */}
       <div className="footer-streak">
